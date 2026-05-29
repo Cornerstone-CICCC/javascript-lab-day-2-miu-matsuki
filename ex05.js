8a9eb6c3e1b2a4e2a50ce19f22d487376a88b192
miu-matsuki <miu.m.works@gmail.com>
2026-05-29 11:17:04 -0700

    ex05.js done

--- ex05.js
+++ ex05.js
@@ -3,3 +3,14 @@
 // Example: countConsonants("hello world") should return 7.
 
 console.log(countConsonants("hello world")); // Expected output: 7
+console.log(countConsonants("hello worldaaaawww")); // Expected output: 10
+
+function countConsonants(str){
+  let consonants = ["a","e","i","o","u"];
+  let cntCon = 0;
+  //console.log(str.split(""));
+
+  return str.split("")
+  .filter((char)=> !consonants.includes(char) && char !== " ").length;
+
+}
