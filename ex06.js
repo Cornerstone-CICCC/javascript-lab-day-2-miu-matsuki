66add2a4957b97cc1ad868f35855b74f79cffb0d
miu-matsuki <miu.m.works@gmail.com>
2026-05-29 11:51:35 -0700

    ex06.js done

--- ex06.js
+++ ex06.js
@@ -4,3 +4,23 @@
 // should return { search: "test", page: "2" }.
 
 console.log(getQueryParams("https://example.com?search=test&page=2")); // Expected output: { search: "test", page: "2" }
+console.log(getQueryParams("https://shop.com?item=apple&price=100&id=999"));
+// output: { item: "apple", price: "100", id: "999" }
+
+
+function getQueryParams(url){
+  let url_para = url.split("?")[1];
+  //console.log(url) //search=test&page=2
+
+  let para =  url_para.split("&");
+  //console.log(para) [ 'item=apple', 'price=100', 'id=999' ]
+
+ let output = {} 
+  for(let i=0; i<para.length; i++){
+    let divied = para[i].split("=");
+    output[divied[0]] = divied[1] ;//{ search: "test", page: "2" }
+    //console.log(divied) ;
+  }
+
+  return output;
+}
