584e9ed14e9d8eff4d0da0a7b4cd7b8e0390ebef
miu-matsuki <miu.m.works@gmail.com>
2026-05-29 10:55:02 -0700

    ex04.js done

--- ex04.js
+++ ex04.js
@@ -2,27 +2,65 @@
 // that appears the most times. If there is a tie, return the first character that reaches the maximum frequency.
 // Example: mostFrequentChar("javascript") should return "a".
 
-console.log(mostFrequentChar("javascript")); // Expected output: "a"
-
-function mostFrequenceChar(word){
-
-  const characters = []; //where i will store my char
-
+function mostFrequentChar(word) {
+  let characters = {};
+  //文字列の中身をひとつづつ見る
+  for( let char of word){
+    if(!characters[char]){
+      characters[char] =0;
+    }
+    characters[char] += 1;
+  }
   console.log(characters);
 
-let mostFrequent = "";
-let currMax = 0;
+  let mostFrequent = "";
+  let currMax = 0;
 
-  for(const chat of word){
+  for(let char of word){
     if(characters[char] > currMax){
       currMax = characters[char];
-      mostFrequent = char; 
+      mostFrequent = char;
     }
   }
-    characters[char];
-  
-
-
-
-
+  // return the final most frequent character
+  return mostFrequent;
 }
+
+console.log(mostFrequentChar('javascript')); // Expected output: "a"
+
+
+
+// function mostFrequentChar(word) {
+//   const characters = {}; // where i will store my characters
+
+//   // iterate over each character of the word param and store it into the object
+//   for (const char of word) {
+//     // if character property does not exist in the object, create a new property
+//     if (!characters[char]) {
+//       characters[char] = 0;
+//     }
+
+//     // increment the value of the character property
+//     characters[char] += 1;
+//   }
+
+//   // see the object on the terminal
+//   console.log(characters);
+
+//   let mostFrequent = ''; // stores the most frequent char
+//   let currMax = 0; // stores the number of times it appears
+
+//   // iterate over each character of the word again but this time check if object property value is greater than the current max
+//   for (const char of word) {
+//     // if character property value is greater than current max, store the number and chracter into the variables
+//     if (characters[char] > currMax) {
+//       currMax = characters[char];
+//       mostFrequent = char;
+//     }
+//   }
+
+//   // return the final most frequent character
+//   return mostFrequent;
+// }
+
+// console.log(mostFrequentChar('javascript')); // Expected output: "a"
