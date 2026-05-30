6bd79895ea342bb20fa250f0a83d0f784d3af699
miu-matsuki <miu.m.works@gmail.com>
2026-05-29 20:39:29 -0700

    ex11.js done

--- ex11.js
+++ ex11.js
@@ -3,3 +3,17 @@
 // Example: countWordOccurrences("hello world hello", "hello") should return 2.
 
 console.log(countWordOccurrences("hello world hello", "hello")); // Expected output: 2
+
+
+function countWordOccurrences(message, keyword){
+
+  const str = message.split(" ");
+
+  return str.filter((word)=> word === keyword).length;
+
+//   console.log(str);
+//   let cnt = 0;
+//   str.map((word)=> word === keyword? cnt+=1 : cnt)
+
+// return cnt;
+}
